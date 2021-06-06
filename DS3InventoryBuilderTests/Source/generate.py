66353aa41a8b527ae499eb6781351be0eb8f1cfe
Phil25 <phil25@protonmail.com>
2021-06-06 08:20:36 +0200

    Group infusions in single test case

--- DS3InventoryBuilderTests/Source/generate.py
+++ DS3InventoryBuilderTests/Source/generate.py
@@ -19,23 +19,30 @@ def to_cpp_identifier(name):
     identifier = identifier.replace("'", "")
     return identifier.replace("-", "")
 
-def generate_test(weapon, infusion, row, suite, out):
+def zero_out(value):
+    return "0" if value == "-" else value
+
+def generate_test(weapon, infusions, suite, out):
     out.write("\n\n")
 
-    out.write(f"TEST_F({suite}, {to_cpp_identifier(weapon)}_{infusion})")
+    out.write(f"TEST_F({suite}, {to_cpp_identifier(weapon)})")
     out.write("{\n")
 
-    out.write(f"const auto& [damage, status] = calculator::AttackRating(db, \"{weapon}\", Weapon::Infusion::{infusion}, 10, attribs);")
+    for infusion, damages in infusions.items():
+        out.write("\t{\n")
+        out.write(f"\tSCOPED_TRACE(\"{infusion}\");\n")
+        out.write(f"\tconst auto& [damage, status] = calculator::AttackRating(db, \"{weapon}\", Weapon::Infusion::{infusion}, 10, attribs);\n")
 
-    out.write(f"EXPECT_EQ({row[Col.PHYSICAL]}, static_cast<int>(damage.physical));")
-    out.write(f"EXPECT_EQ({row[Col.MAGIC]}, static_cast<int>(damage.magic));")
-    out.write(f"EXPECT_EQ({row[Col.FIRE]}, static_cast<int>(damage.fire));")
-    out.write(f"EXPECT_EQ({row[Col.LIGHTNING]}, static_cast<int>(damage.lightning));")
-    out.write(f"EXPECT_EQ({row[Col.DARK]}, static_cast<int>(damage.dark));")
+        out.write(f"\tEXPECT_EQ({damages['physical']}, static_cast<int>(damage.physical));\n")
+        out.write(f"\tEXPECT_EQ({damages['magic']}, static_cast<int>(damage.magic));\n")
+        out.write(f"\tEXPECT_EQ({damages['fire']}, static_cast<int>(damage.fire));\n")
+        out.write(f"\tEXPECT_EQ({damages['lightning']}, static_cast<int>(damage.lightning));\n")
+        out.write(f"\tEXPECT_EQ({damages['dark']}, static_cast<int>(damage.dark));\n")
 
-    out.write(f"EXPECT_EQ({row[Col.BLEED]}, static_cast<int>(status.bleed));")
-    out.write(f"EXPECT_EQ({row[Col.POISON]}, static_cast<int>(status.poison));")
-    out.write(f"EXPECT_EQ({row[Col.FROST]}, static_cast<int>(status.frost));")
+        out.write(f"\tEXPECT_EQ({damages['bleed']}, static_cast<int>(status.bleed));\n")
+        out.write(f"\tEXPECT_EQ({damages['poison']}, static_cast<int>(status.poison));\n")
+        out.write(f"\tEXPECT_EQ({damages['frost']}, static_cast<int>(status.frost));\n")
+        out.write("\t}\n")
 
     out.write("}")
 
@@ -54,6 +61,8 @@ def generate_suite(suite, attribs, filename):
         out.write(f"Database {suite}::db = Database::Create();\n")
         out.write(f"PlayerAttributes {suite}::attribs = {{{attribs}}};\n")
 
+        weapons = {}
+
         with open(f"./Data/{filename}", "r") as f:
             reader = csv.reader(f, delimiter="\t")
             for row in reader:
@@ -70,7 +79,22 @@ def generate_suite(suite, attribs, filename):
                     infusion = "None"
                     weapon = "Dark Hand"
 
-                generate_test(weapon, infusion, [col.replace("-", "0") for col in row], suite, out)
+                if weapon not in weapons:
+                    weapons[weapon] = {}
+
+                weapons[weapon][infusion] = {
+                    "physical": zero_out(row[Col.PHYSICAL]),
+                    "magic": zero_out(row[Col.MAGIC]),
+                    "fire": zero_out(row[Col.FIRE]),
+                    "lightning": zero_out(row[Col.LIGHTNING]),
+                    "dark": zero_out(row[Col.DARK]),
+                    "bleed": zero_out(row[Col.BLEED]),
+                    "poison": zero_out(row[Col.POISON]),
+                    "frost": zero_out(row[Col.FROST]),
+                }
+
+        for weapon, infusions in weapons.items():
+            generate_test(weapon, infusions, suite, out)
 
 for filename in os.listdir("./Data/"):
     split = filename.split("-")
