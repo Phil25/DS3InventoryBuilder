59b4dee1d6b65db2b20b90ec378be165c5b35c88
Phil25 <phil25@protonmail.com>
2021-06-11 20:10:14 +0200

    Set max limit for infusion and stat icon size

--- DS3InventoryBuilder/Source/Utils/DrawWeapon.cpp
+++ DS3InventoryBuilder/Source/Utils/DrawWeapon.cpp
@@ -8,19 +8,19 @@ using DB = invbuilder::Database;
 void DrawWeapon(wxDC* dc, const int size, const std::string& name, const Infusion infusion, const wxPoint& pos, const int missingRequirements)
 {
 	dc->DrawRectangle(pos, {size, size});
-
 	dc->DrawBitmap(wxGetApp().GetImage(name, size), pos, false);
+
+	const auto iconSize = std::min(size / 4, 30);
 
 	if (infusion != Infusion::None)
 	{
-		const int infusionSize = size / 4;
-		const int offset = size - infusionSize - 3;
-		dc->DrawBitmap(wxGetApp().GetImage(DB::ToString(infusion), infusionSize), pos.x + offset, pos.y + offset, false);
+		const int offset = size - iconSize - 3;
+		dc->DrawBitmap(wxGetApp().GetImage(DB::ToString(infusion), iconSize), pos.x + offset, pos.y + offset, false);
 	}
 
 	switch (missingRequirements)
 	{
-	case 0: dc->DrawBitmap(wxGetApp().GetImage("NoStats", size / 4), pos.x + size - size / 4 - 2, pos.y + 2, false); break;
-	case 1: dc->DrawBitmap(wxGetApp().GetImage("TwoHanded", size / 4), pos.x + size - size / 4 - 2, pos.y + 2, false); break;
+	case 0: dc->DrawBitmap(wxGetApp().GetImage("NoStats", iconSize), pos.x + size - iconSize - 2, pos.y + 2, false); break;
+	case 1: dc->DrawBitmap(wxGetApp().GetImage("TwoHanded", iconSize), pos.x + size - iconSize - 2, pos.y + 2, false); break;
 	}
 }
